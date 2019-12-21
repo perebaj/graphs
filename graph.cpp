595f4e07a523f7408f3d707cc618998b20f0a9f5
perebaj <perebaj@gmail.com>
2019-12-21 12:11:25 -0300

    Pronto e funcionante, apenas documentar e uns loops imbecis

--- graph.cpp
+++ graph.cpp
@@ -3,6 +3,12 @@ Jonathan Santos Silva RA:759052 Engenharia de Computação UFSCAR
 */
 #include <iostream>
 #include <vector>
+#include <queue>
+
+#define MAX 10100
+#define INFINITO 999999999
+typedef std::pair <int, int> pii;
+
 typedef struct vertice 
 {
     int           rotulo;
@@ -88,24 +94,78 @@ void readData(char *fileName, Arestas *arestas, Grafo *grafo)
     }
 }
 
+int sizeDe(Vertice *vertice){
+    int count = 0;
+    for(int iterator = 0; vertice != nullptr; vertice = vertice->prox)
+        count ++;
+    
+    return count;
+}
+
+int Dijskstra(int origem, int destino, Grafo *grafo, Vertice *vertice){
+    int distancia[MAX];
+    int processado[MAX];
+    std::vector<pii> vizinhos[MAX];
+    for(int iterator = 0; iterator < grafo->numVertices; iterator ++){
+        distancia[iterator] = INFINITO;
+        distancia[origem] = 0;
+    }
+
+    std::priority_queue<pii, std::vector<pii>, std::greater<pii> > fila;
+    fila.push(pii(distancia[origem], origem));
+
+    while(true){
+        int davez = -1;
+        int menor = INFINITO;
+
+        while(!fila.empty()){
+            int atual = fila.top().second;
+            fila.pop();
+
+            if(!processado[atual]){
+                davez = atual;
+                break;
+            }
+
+        }
+
+        if(davez == -1) break;
+
+        processado[davez] = true;
+        vertice = grafo->adj[davez];
+        
+        for(vertice = grafo->adj[davez]; vertice != nullptr; vertice = vertice->prox){
+            int dist = vertice->weight;
+            int atual = vertice->rotulo;
+
+            if(distancia[atual] > distancia[davez] + dist){
+                distancia[atual] = distancia[davez] + dist;
+                fila.push(pii(distancia[atual], atual));
+            }
+        }
+
+    }
+ 
+    return distancia[destino];
+
+}
+
+
+
 int main()
 {
     char fileName[100];
     scanf("%s", fileName);
     Arestas *arestas = new Arestas;
     Grafo *grafo = new Grafo;
+    Vertice *vertice  = new Vertice;
 
     readData(fileName, arestas, grafo); 
     grafo = inicializaGrafo(grafo->numVertices,grafo->numArestas);
     criarGrafo(grafo,arestas->arestas);
-    printGrafo(grafo);         
+    printGrafo(grafo);
+    
+    int way = Dijskstra(0, grafo->numVertices - 1, grafo, vertice);
+    std::cout << way << std::endl;
 }
-
-
-
-
-
-
-
-
 
